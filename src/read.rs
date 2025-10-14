6e30b81c8dd45beb5e520519f01787e867a042f9
viell <me@viell.dev>
2025-10-14 16:09:01 +0200

    Added Read impl, cleared the trait for now, also removed temp.rs, no idea when I accidentally submitted that junk

--- src/read.rs
+++ src/read.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead};
+use std::io::{self, Read};
 
 /// A `DynBufRead` is an alternative to `BufRead` with a buffer that can grow and shrink in size as
 /// needed. This allows for peeking far into this source without by growing the buffer as needed.
@@ -7,9 +7,4 @@ use std::io::{self, BufRead};
 /// TODO: More docs
 ///
 /// [`compact()`]: DynBufRead::compact
-pub trait DynBufRead: BufRead {
-    fn ensure_available(&mut self, min_bytes: usize) -> io::Result<usize>;
-    fn peek(&mut self, n: usize) -> io::Result<&[u8]>;
-    fn available(&self) -> usize;
-    fn compact(&mut self);
-}
+pub trait DynBufRead: Read {}
