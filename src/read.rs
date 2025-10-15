17067585781f1cf2c90d4d87976c310f6af5ae3b
viell <me@viell.dev>
2025-10-15 11:07:20 +0200

    Added Claude generated tests for now, will replace later

--- src/read.rs
+++ src/read.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead};
+use std::io::BufRead;
 
 /// A `DynBufRead` is an alternative to `BufRead` with a buffer that can grow and shrink in size as
 /// needed. This allows for peeking far into this source without by growing the buffer as needed.
@@ -7,4 +7,9 @@ use std::io::{self, BufRead};
 /// TODO: More docs
 ///
 /// [`compact()`]: DynBufRead::compact
-pub trait DynBufRead: BufRead {}
+pub trait DynBufRead: BufRead {
+    // TODO:
+    // fn consume(&mut self, amt: usize);
+    // fn discard(&mut self);
+    // fn compact(&mut self);
+}
